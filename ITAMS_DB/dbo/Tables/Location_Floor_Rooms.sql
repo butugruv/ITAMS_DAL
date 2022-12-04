f6a82662bc67f0be7a7aeb152d85f691144ec240
Chris <butugruv@gmail.com>
2022-12-04 11:25:41 -1000

    p

--- ITAMS_DB/dbo/Tables/Location_Floor_Rooms.sql
+++ ITAMS_DB/dbo/Tables/Location_Floor_Rooms.sql
@@ -3,5 +3,5 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [location_floor_id] INT NOT NULL, 
     [room] NVARCHAR(50) NOT NULL, 
-    CONSTRAINT [FK_Location_Floor_Rooms_Location_Floor] FOREIGN KEY (location_floor_id) REFERENCES Location_Floors(id)
+    CONSTRAINT [FK_Location_Floor_Rooms_Location_Floor] FOREIGN KEY (location_floor_id) REFERENCES Location_Floors(Id)
 )
