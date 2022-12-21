9f1c2172237573a11c228e68e380a3c21bd13a34
Chris <butugruv@gmail.com>
2022-12-20 19:39:47 -1000

    update device_type_counts_vw

--- ITAMS_DB/dbo/Views/Device_Type_Counts_vw.sql
+++ ITAMS_DB/dbo/Views/Device_Type_Counts_vw.sql
@@ -1,7 +1,7 @@
 ﻿CREATE VIEW [dbo].[DeviceType_Counts_vw]
 AS 
-select dtype.TypeName
-	,count(*) as "Count"
-from Devices devi
-inner join Device_Types dtype on devi.DeviceTypeId = dtype.Id
-group by dtype.TypeName
+select dtype.TypeName, 
+	count(devi.DeviceName) as "Count"
+from Device_Types dtype
+left join Devices devi on devi.DeviceTypeId = dtype.Id
+group by dtype.typename
